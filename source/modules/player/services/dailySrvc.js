87c5180a7c8add6cb2f291e6cc5d49a39a7f4732
Devon Maloney <devon.maloney@vokal.io>
2015-10-06 16:13:43 -0500

    Add support for different days view

--- source/modules/player/services/dailySrvc.js
+++ source/modules/player/services/dailySrvc.js
@@ -1,102 +1,35 @@
 angular.module( "Player" )
 
-.service( "DailySrvc", [ "APISrvc", "$q", "$timeout",
-    function ( APISrvc, $q, $timeout )
+.service( "DailySrvc", [ "APISrvc",
+    function ( APISrvc )
     {
         "use strict";
 
-        var dailyList = [];
-        var dailyPromise = $q.defer();
-
-        function indexBySteamId( id )
+        var dailyKeys = [ "kills", "deaths", "dominations", "dominated", "revenges" ];
+        this.getAll = function ( days )
         {
-            for( var i = 0; i < dailyList.length; i++ )
-            {
-                if( dailyList[ i ].STEAMID === id )
+            days = days || "1";
+            return APISrvc.$get( "/stats-over-days/" + days )
+                .then( function ( res )
                 {
-                    return i;
-                }
-            }
-            return null;
-        }
-
-        this.getAll = function ()
-        {
-            return dailyPromise.promise.then( function ()
-            {
-                return dailyList;
-            } );
-        };
-        this.getPlayer = function ( steamId )
-        {
-            var deferred = $q.defer();
-            dailyPromise.promise.then( function ()
-            {
-                for( var i = 0; i < dailyList.length; i++ )
-                {
-                    if( dailyList[ i ].STEAMID === steamId )
+                     return Object.keys( res.data ).map( function ( id )
                     {
-                        deferred.resolve( dailyList[ i ] );
-                        break;
-                    }
-                }
-                deferred.reject( "Cannot find stats by Steam ID: " + steamId );
-                return dailyList;
-            } );
-            return deferred.promise;
+                        res.data[ id ].STEAMID = id;
+                        dailyKeys.forEach( function ( key )
+                        {
+                            if( !res.data[ id ][ key ] )
+                            {
+                                res.data[ id ][ key ] = 0;
+                            }
+                        } );
+                        return res.data[ id ];
+                    } ).filter( filterNullIds );
+                } );
         };
-
-        APISrvc.$get( "/stats-over-days/1" )
-            .then( function ( res )
-            {
-                Object.keys( res.data ).map( function ( id )
-                {
-                    res.data[ id ].STEAMID = id;
-                    return res.data[ id ];
-                } ).filter( filterNullIds ).forEach( insertDailyStats );
-
-                dailyPromise.resolve( true );
-                pollDaily();
-            }, function ( err )
-            {
-                dailyPromise.reject( "Could not retrieve daily stats: " + err.status );
-            } );
 
         function filterNullIds( player )
         {
             return !!player.STEAMID;
-        }
-        function pollDaily()
-        {
-            $timeout( function ()
-            {
-                APISrvc.$get( "/stats-over-days/1" )
-                    .then( function ( res )
-                    {
-                        Object.keys( res.data ).map( function ( id )
-                        {
-                            res.data[ id ].STEAMID = id;
-                            return res.data[ id ];
-                        } ).filter( filterNullIds ).forEach( insertDailyStats );
-                    } )
-                    .finally( function ()
-                    {
-                        pollDaily();
-                    } );
-
-            }, ( 5 * 1000 ) );
-        }
-        function insertDailyStats( player )
-        {
-            var index = indexBySteamId( player.STEAMID );
-            if( angular.isNumber( index ) )
-            {
-                angular.merge( dailyList[ index ], player );
-            }
-            else if( player.STEAMID )
-            {
-                dailyList.push( player );
-            }
         }
 
         return this;
